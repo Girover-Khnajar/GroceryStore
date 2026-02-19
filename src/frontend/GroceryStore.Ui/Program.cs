265b142227b2e0a1417e7c91f6e2bf5ccbac9a1d
Juan Khanjar <jan4ma@gmail.com>
2026-02-19 07:57:02 +0100

    UI:add Ui Skeleton

--- src/frontend/GroceryStore.Ui/Program.cs
+++ src/frontend/GroceryStore.Ui/Program.cs
@@ -1,24 +1,31 @@
+using GroceryStore.Ui.Services;
 using GroceryStore.Ui.Components;
+
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddRazorComponents()
-    .AddInteractiveServerComponents();
+// Add services to the container.
+builder.Services.AddRazorComponents( );
+//builder.Services.AddRazorComponents( )
+//    .AddInteractiveServerComponents( ); // Interactive SSR (Server)
+builder.Services.Configure<ApiOptions>(builder.Configuration.GetSection(ApiOptions.SectionName));
+builder.Services.AddHttpClient<IApiClient,ApiClient>( );
+var app = builder.Build( );
 
-var app = builder.Build();
-
-if (!app.Environment.IsDevelopment())
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment( ))
 {
-    app.UseExceptionHandler("/Error", createScopeForErrors: true);
-    app.UseHsts();
+    app.UseExceptionHandler("/Error",createScopeForErrors: true);
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts( );
 }
-app.UseStatusCodePagesWithReExecute("/not-found", createScopeForStatusCodePages: true);
-app.UseHttpsRedirection();
+app.UseStatusCodePagesWithReExecute("/not-found",createScopeForStatusCodePages: true);
+app.UseHttpsRedirection( );
 
-app.UseAntiforgery();
+app.UseAntiforgery( );
 
-app.MapStaticAssets();
-app.MapRazorComponents<App>()
-    .AddInteractiveServerRenderMode();
-
-app.Run();
+app.MapStaticAssets( );
+app.MapRazorComponents<App>( );
+//app.MapRazorComponents<App>( )
+//   .AddInteractiveServerRenderMode( );
+app.Run( );
